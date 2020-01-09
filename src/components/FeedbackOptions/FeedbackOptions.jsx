2bebe9b1eddfaf111b539eaaef1572c6b8c9d093
Marina <marfen24@i.ua>
2020-01-09 22:17:44 +0200

    fixed

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -2,18 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Styles from './FeedbackOptions.module.css';
 
-const FeedbackOptions = ({ option, onLeaveFeedback, name }) => {
-  return (
-    <button
-      className={Styles.feedbackButtons}
-      name={name}
-      onClick={onLeaveFeedback}
-      type="button"
-    >
-      {option}
-    </button>
-  );
-};
+const FeedbackOptions = ({ option, onLeaveFeedback, name }) => (
+  <button
+    className={Styles.feedbackButtons}
+    name={name}
+    onClick={onLeaveFeedback}
+    type="button"
+  >
+    {option}
+  </button>
+);
 
 FeedbackOptions.propTypes = {
   option: PropTypes.string.isRequired,
